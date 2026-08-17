e15e884c777387e5ef98b71e559547bab972f622
Raju Kumar Pandey <pandeyraju071@gmail.com>
2026-08-17 12:19:48 +0530

    Area of circle

---
+++ src/assignment/conditionals_loops/AreaCircle.java
@@ -0,0 +1,19 @@
+package assignment.conditionals_loops;
+
+import java.util.Scanner;
+
+public class AreaCircle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle");
+        double radius = in.nextDouble();
+        double result = circumference(radius);
+        System.out.println("The area of the circle is " + result);
+    }
+    public static double circumference(double radius) {
+        double area = radius * radius;
+        return area;
+    }
+
+}
